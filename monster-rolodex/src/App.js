b500f02e1a26bdbbe720968d92607d94d220007f
ashritha369 <ashrithagowda666@gmail.com>
2023-02-01 20:56:36 +0530

    Understanding of storing original data

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  // LIFE CYCLE METHODS
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+    console.log("filteredMonsters:", filteredMonsters);
+    console.log("After render");
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log("After return");
+            console.log({ startingArray: this.state.monsters });
+            const searchField = event.target.value.toLowerCase();
+
+            this.setState(
+              () => {
+                return { searchField };
+              },
+              () => {
+                console.log({ endingArray: this.state.monsters });
+              }
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ monster-rolodex/src/document.md
@@ -0,0 +1,5 @@
+# Understanding of Storage of original data
+
+![Image](../src/Imgs/1.png)
+![Image](../src/Imgs/2.png)
+![Image](../src/Imgs/3.png)
